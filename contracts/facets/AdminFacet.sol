81df827c8c6a31094727a5f348a7becb1de4ef28
justwei <admin@taylorswiftcn.com>
2023-03-29 23:05:52 +0800

    add withdrawFees function

--- contracts/facets/AdminFacet.sol
+++ contracts/facets/AdminFacet.sol
@@ -51,6 +51,11 @@ contract AdminFacet is IAdminFacet {
         emit Events.WithdrawLotteryPot(msg.sender, _to, _value);
     }
 
+    function withdrawFees(address _to, uint _value) external {
+        LibDiamond.enforceIsContractOwner();
+        payable(_to).transfer(_value);
+    }
+
     function randomizerWithdraw(address _user, uint256 _amount) external {
         LibDiamond.enforceIsContractOwner();
         IRandomizer(s.randomizer).clientWithdrawTo(_user, _amount);
